014458ccc1450273f1267959c7d7fcd5a2bceea0
Raad <raadsn11@gmail.com>
2023-05-23 13:36:10 +0200

    neww

---
+++ loops/numbers.js
@@ -0,0 +1,45 @@
+function up(n){
+
+	n = parseInt(prompt("enter"));
+
+	for (var i = 0; i <= n; i++) {
+
+		console.log(i);
+	}
+
+}
+
+
+function des(n){
+// descending order
+	for (var i = n; i >= 0; i--) {
+	
+		console.log(i);
+	}	
+
+}
+
+
+
+ //-n to n 
+let n = parseInt(prompt("enter"));             
+
+function zero (n) {
+																																	 // <<----------------------- SHorter way-------------->>>
+	for ( i = -n; i <= n; i++) {
+
+		console.log(i);
+	 }
+
+}
+
+zero (n);
+
+// for (i = 100 * -1 ; i <= 0; i++) {  																							 <<--------	another way as well----->>        
+
+// 	console.log(i);
+
+// 	if (i === 0) {
+// 		zero(i);
+// 	}
+// }
---
+++ varibals/strings.js
@@ -0,0 +1,7 @@
+
+function two(text1,text2) {
+
+	console.log(text1,text2);
+}
+
+two("hello,","raad");
